19a190d057b7b8fac523f32738676100af131ed0
Muhammed Kocabas <muhammed.kocabas@outlook.com>
2024-11-10 01:42:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ See LICENCE.txt for licensing and contact information.
 """
 
 from distutils.core import setup
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+# try: # for pip >= 10
+#     from pip._internal.req import parse_requirements
+# except ImportError: # for pip <= 9.0.3
+#     from pip.req import parse_requirements
 from runpy import run_path
 
-install_reqs = parse_requirements('requirements.txt', session=False)
-try:  # for pip < 20.1
-    install_requires = [str(ir.req) for ir in install_reqs]
-except AttributeError:  # for pip >= 20.1
-    install_requires = [str(ir.requirement) for ir in install_reqs]
+# install_reqs = parse_requirements('requirements.txt', session=False)
+# try:  # for pip < 20.1
+#     install_requires = [str(ir.req) for ir in install_reqs]
+# except AttributeError:  # for pip >= 20.1
+#     install_requires = [str(ir.requirement) for ir in install_reqs]
 
 def get_version():
     namespace = run_path('chumpy/version.py')
@@ -29,7 +29,7 @@ setup(name='chumpy',
     url='https://github.com/mattloper/chumpy',
     description='chumpy',
     license='MIT',
-    install_requires=install_requires,
+    # install_requires=install_requires,
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
